4e3cc8b23c64d34edf70b4706cc1bfbe5d1b5409
Buter <aleksbutav@gmail.com>
2023-06-16 13:47:36 +0300

    improve: added phpDoc to models

--- app/models/Task.php
+++ app/models/Task.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use DateTime;
 use Illuminate\Database\Eloquent\Model as Eloquent;
 
+/**
+ * @property int $id
+ * @property DateTime $created_at
+ * @property DateTime $updated_at
+ * @property string $name
+ * @property string $email
+ * @property string $body
+ * @property bool $done
+ * @property bool $edited
+ */
 class Task extends Eloquent
 {
     protected $fillable = ['email', 'name', 'body', 'edited'];
+
+    protected $casts = [
+        'id' => 'integer',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'done' => 'boolean',
+        'edited' => 'boolean',
+    ];
 }
--- app/models/User.php
+++ app/models/User.php
@@ -4,9 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model as Eloquent;
 
+/**
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property bool $is_admin
+ */
 class User extends Eloquent
 {
     public $timestamps = [];
 
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $casts = [
+        'is_admin' => 'boolean',
+    ];
 }
